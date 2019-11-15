b01829f636edd83e93dc0dbe58060458fa0b6eac
mariazinchyn <mariazinchyn@gmail.com>
2019-11-15 20:17:27 +0200

    subcategoryResponse

--- src/main/java/com/mariazinchyn/myshop/dto/response/SubcategoryResponse.java
+++ src/main/java/com/mariazinchyn/myshop/dto/response/SubcategoryResponse.java
@@ -1,4 +1,24 @@
 package com.mariazinchyn.myshop.dto.response;
 
+import com.mariazinchyn.myshop.entity.Subcategory;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class SubcategoryResponse {
+    private Long id;
+    private String name;
+    private Long categoryId;
+
+    public SubcategoryResponse(Subcategory subcategory){
+        id = subcategory.getId();
+        name = subcategory.getName();
+        categoryId = subcategory.getCategory().getId();
+
+    }
+
+
+
 }
